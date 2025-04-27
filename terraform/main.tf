44c6cc131d14c3030bac555bd0be3225d83c9050
Hg347 <c.hgdorn+github@hg42.de>
2025-04-27 20:07:29 +0200

    added tags

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,7 @@
+#
+# ctf-rush, AWS environment
+# terraform state file is remotely in an S3 bucket
+#
 terraform {
   backend "s3" {
     bucket         = "ctfrush-terraform-state" 
@@ -11,14 +15,23 @@ provider "aws" {
   region = "eu-central-1"
 }
 
+variable "ctf_tags" {
+  default = {
+    ctf:creator = "terraform"
+  }
+}
+
+
 resource "aws_vpc" "main" {
   cidr_block = "10.0.0.0/16"
+  tags = var.ctf_tags
 }
 
 resource "aws_subnet" "public_subnet" {
   vpc_id            = aws_vpc.main.id
   cidr_block        = "10.0.1.0/24"
   map_public_ip_on_launch = true
+  tags = var.ctf_tags
 }
 
 output "vpc_id" {
