e8744df3ff0b1d46da78dc1b5fdf1621ebf297c5
Hg347 <c.hgdorn+github@hg42.de>
2025-05-17 19:31:55 +0200

    fixed terraform aws credentials

--- terraform/main.tf
+++ terraform/main.tf
@@ -18,6 +18,10 @@ terraform {
 }
 
 
+#
+# Note 15.05.2025: pipeline does not know a profile, this is saved in local aws settings only
+# the pipeline works with secrets and variables
+#
 provider "aws" {
   region = var.aws_settings.region
   profile = "terraform" # AWS profile with the correct credentials
